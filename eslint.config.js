1e3e5b7a6e44a53bf8ad38d5c9756e9149e38e66
Muhammad Irfan Kurniawan <prostiate@gmail.com>
2025-12-02 14:35:41 +0700

    feat: migrate to ESLint 9 flat config format

---
+++ eslint.config.js
@@ -0,0 +1,86 @@
+const reactNative = require('@react-native/eslint-config');
+const typescriptParser = require('@typescript-eslint/parser');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+const reactPlugin = require('eslint-plugin-react');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'android/**',
+      'ios/**',
+      '.expo/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      '*.config.js',
+      '.eslintrc.js',
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        __DEV__: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        Promise: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      react: reactPlugin,
+      'react-native': reactNativePlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+
+      // React rules
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // React Native rules
+      'react-native/no-inline-styles': 'warn',
+
+      // React Hooks rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // General rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-undef': 'off', // TypeScript handles this
+    },
+  },
+  // Prettier config should be last to override other formatting rules
+  prettierConfig,
+];
