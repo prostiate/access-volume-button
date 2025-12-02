35c62bdc6248b0568687aaf8e59de456bad842b9
Muhammad Irfan Kurniawan <prostiate@gmail.com>
2025-12-02 13:59:21 +0700

    chore: add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  rules: {
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    
+    // React rules
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  env: {
+    'react-native/react-native': true,
+  },
+};
