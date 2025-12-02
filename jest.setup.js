06c038fad216332b69e7b394d158f736c544a824
Muhammad Irfan Kurniawan <prostiate@gmail.com>
2025-12-02 14:14:53 +0700

    test: add Jest setup configuration

---
+++ jest.setup.js
@@ -0,0 +1,40 @@
+// Jest setup file for React Native Testing Library
+import '@testing-library/react-native/extend-expect';
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+// Mock react-native-vector-icons
+jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'Icon');
+jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon');
+
+// Mock react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+// Mock react-native-paper
+jest.mock('react-native-paper', () => {
+  const RealModule = jest.requireActual('react-native-paper');
+  return {
+    ...RealModule,
+    Provider: ({ children }) => children,
+  };
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+// Global test timeout
+jest.setTimeout(10000);
+
+// Suppress console warnings in tests
+global.console = {
+  ...console,
+  warn: jest.fn(),
+  error: jest.fn(),
+};
